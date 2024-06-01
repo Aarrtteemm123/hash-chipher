1deac158cd6aaafb69813ae9ff7b42f71653e478
Artem <aarrtteemm258013@gmail.com>
2024-06-02 02:44:39 +0300

    refactoring code, release hash_cipher_v1

---
+++ hash_cipher_v1.py
@@ -0,0 +1,87 @@
+import os
+import random
+from typing import List, Dict, Tuple
+import lz4framed
+import xxhash
+
+
+class HashCipherV1:
+    def code(self, data: bytes, compress: bool = False) -> Tuple[List[str], str]:
+        hex_list = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f')
+        hash_table = [None] * 256
+        counter = 0
+
+        for hex_ in hex_list:
+            for hex_2 in hex_list:
+                while True:
+                    hash_ = os.urandom(8).hex()  # get 8*2=16 str size
+                    hash_2 = xxhash.xxh3_64_hexdigest(hash_ + hex_2)
+                    if hash_2[0] == hex_:
+                        hash_table[counter] = hash_
+                        counter += 1
+                        break
+
+        if compress:
+            data = lz4framed.compress(data)
+
+        data = data.hex()
+        len_data = len(data)
+        max_hight_val = int(len_data * 2)
+        min_hight_val = 1000
+        max_min_len = int(len_data * 0.1)
+        min_min_len = 100
+        tail_max_len = max_hight_val if max_hight_val > min_hight_val else min_hight_val
+        tail_min_len = max_min_len if max_min_len > min_min_len else min_min_len
+        tail = random.randint(tail_min_len, tail_max_len)
+        random_bytes = os.urandom(len_data + tail).hex()
+        hex_decimal_dict = {'0' + hex(i)[2:] if i < 16 else hex(i)[2:]: i for i in range(256)}
+        secret_list = [hash_table[hex_decimal_dict[data[i] + random_bytes[i]]] for i in range(len_data)]
+        return secret_list, random_bytes
+
+    def decode(self, secret_keys: List[bytes], public_key: bytes) -> bytes:
+        compressed = secret_keys[-1] == b'1'
+        secret_keys.pop(-1)
+        public_key_str = public_key.decode()
+
+        hash_parts = [
+            xxhash.xxh3_64_hexdigest(secret_key + public_key_str[index].encode())[0] for index, secret_key in
+            enumerate(secret_keys)
+        ]
+
+        decode_msg = ''.join(hash_parts)
+        decode_msg = bytes.fromhex(decode_msg)
+
+        if compressed:
+            decode_msg = lz4framed.decompress(decode_msg)
+
+        return decode_msg
+
+    def save_keys(self, secret_keys: List[str] = None, public_key: str = None, compressed: bool = False,
+                  filename_secret_key: str = 'secret_key_v1.key',
+                  filename_public_key: str = 'public_key_v1.key'):
+
+        if secret_keys:
+            write_data = ('\n'.join(secret_keys) + f'\n{int(compressed)}').encode()
+            self._save_file(write_data, filename_secret_key)
+        if public_key:
+            self._save_file(public_key.encode(), filename_public_key)
+
+    def load_keys(self, filename_secret_key: str = None, filename_public_key: str = None) -> Dict[str, bytes]:
+        keys = {}
+        if filename_secret_key:
+            secret_key = self._read_file(filename_secret_key).splitlines()
+            keys['secret_key'] = secret_key
+        if filename_public_key:
+            public_key = self._read_file(filename_public_key)
+            keys['public_key'] = public_key
+
+        return keys
+
+    def _read_file(self, path: str) -> bytes:
+        with open(path, 'rb') as file:
+            data = file.read()
+        return data
+
+    def _save_file(self, data: bytes, path: str):
+        with open(path, 'wb') as file:
+            file.write(data)
--- main.py
+++ main.py
@@ -1,103 +1,33 @@
-import os
-import random
 import time
-from typing import List
 
-import lz4framed
-import xxhash
+from hash_cipher_v1 import HashCipherV1
 
 
-def read_file(path: str):
+def read_file(path: str) -> bytes:
     with open(path, 'rb') as file:
         data = file.read()
     return data
 
 
-def write_file(data: bytes, path: str):
-    with open(path, 'wb') as file:
-        file.write(data)
-
-
-def save_keys(secret_keys: List[str], public_key: str, compressed: bool):
-    with open('secret_key.key', 'wb') as file:
-        write_data = ('\n'.join(secret_keys) + f'\n{int(compressed)}').encode()
-        file.write(write_data)
-    with open('public_key.key', 'wb') as file:
-        file.write(public_key.encode())
-
-
-def decode(secret_keys: List[bytes], public_key: bytes):
-    compressed = secret_keys[-1] == b'1'
-    secret_keys.pop(-1)
-    public_key_str = public_key.decode()
-
-    hash_parts = [
-        xxhash.xxh3_64_hexdigest(secret_key + public_key_str[index].encode())[0] for index, secret_key in
-        enumerate(secret_keys)
-    ]
-
-    decode_msg = ''.join(hash_parts)
-    decode_msg = bytes.fromhex(decode_msg)
-
-    if compressed:
-        decode_msg = lz4framed.decompress(decode_msg)
-
-    return decode_msg
-
-
-def code(data: bytes, compress=False):
-    hex_list = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f')
-    hash_table = [None] * 256
-    counter = 0
-
-    for hex_ in hex_list:
-        for hex_2 in hex_list:
-            while True:
-                hash_ = os.urandom(8).hex()  # get 8*2=16 str size
-                hash_2 = xxhash.xxh3_64_hexdigest(hash_ + hex_2)
-                if hash_2[0] == hex_:
-                    hash_table[counter] = hash_
-                    counter += 1
-                    break
-
-    if compress:
-        data = lz4framed.compress(data)
-
-    data = data.hex()
+def benchmark(func, data, info_msg, *args, **kwargs):
     len_data = len(data)
-    max_hight_val = int(len_data * 2)
-    min_hight_val = 1000
-    max_min_len = int(len_data * 0.1)
-    min_min_len = 100
-    tail_max_len = max_hight_val if max_hight_val > min_hight_val else min_hight_val
-    tail_min_len = max_min_len if max_min_len > min_min_len else min_min_len
-    tail = random.randint(tail_min_len, tail_max_len)
-    random_bytes = os.urandom(len_data + tail).hex()
-    hex_decimal_dict = {'0' + hex(i)[2:] if i < 16 else hex(i)[2:]: i for i in range(256)}
-    secret_list = [hash_table[hex_decimal_dict[data[i] + random_bytes[i]]] for i in range(len_data)]
+    start_time = time.time()
+    res = func(*args, **kwargs)
+    end_time = time.time()
+    total_time = end_time - start_time
+    data_size_MB = len_data / (1024 * 1024)  # Convert bytes to Megabytes
+    throughput = data_size_MB / total_time if total_time else float('inf')
+    print(f'---------------{info_msg}---------------')
+    print(f"Total time taken: {total_time} seconds")
+    print(f"Throughput: {throughput} MB/s")
+    print(f"File size: {data_size_MB} MB\n")
+    return res
 
-    return secret_list, random_bytes
 
-
-data = read_file('data/dummy-500-kb-example-png-file.png')
-# data = b'Hello World!'
-len_data = len(data)
-start_total_time = time.time()
-secret_key, public_key = code(data, False)
-end_total_time = time.time()
-total_time = end_total_time - start_total_time
-data_size_MB = len_data / (1024 * 1024)  # Convert bytes to Megabytes
-throughput = data_size_MB / total_time
-# save_keys(secret_key, public_key, False)
-# secret_key = read_file('secret_key.key').splitlines()
-# public_key = read_file('public_key.key')
-# st_decode = time.time()
-# msg = decode(secret_key, public_key)
-# end_decode = time.time()
-# total_time_decode = end_decode - st_decode
-# throughput_decode = data_size_MB / (total_time_decode + 0.00000001)
-print(f"Total time code taken: {total_time} seconds")
-print(f"Throughput code: {throughput} MB/s")
-# print(f"Total time decode taken: {total_time_decode} seconds")
-# print(f"Throughput decode: {throughput_decode} MB/s")
-print(f"File size: {data_size_MB} MB")
+data = read_file('data/1mb.json')
+hash_cipher = HashCipherV1()
+#data = b'Hello World!'
+secret_key, public_key = benchmark(hash_cipher.code, data, 'code', data)
+hash_cipher.save_keys(secret_key, public_key)
+secret_key, public_key = hash_cipher.load_keys('secret_key_v1.key', 'public_key_v1.key').values()
+msg = benchmark(hash_cipher.decode, secret_key, 'decode', secret_key, public_key)
